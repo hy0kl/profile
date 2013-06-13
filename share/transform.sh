71bc56e64ad693f674ab7a42e75647e07cadaf27
Jerry Yang <hy0kle@gmail.com>
2013-06-13 17:31:58 +0800

    new shell script.

---
+++ share/transform.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# @author:   Jerry Yang(hy0kle@gmail.com)
+# @describe: 批量将源代码格式化
+# 1. 将文件转为 Unix 风格
+# 2. 将 tab 替换成 4 个空格
+# 3. 删除行尾的空格
+# 批量有风险
+
+# CTRL + v + TAB 
+# tab 的输入方法
+
+#set -x
+
+work_path="/Users/hy0kl/nginx/svn-work/trunk"
+date_str=$(date +"%Y-%m-%d")
+transfer_file="php tpl js css"
+
+cd $work_path
+mkdir -p tmp
+
+for file_type in $transfer_file
+do
+    tmp_file="tmp/$file_type.$date_str"
+    find ./ -name "*.$file_type" > $tmp_file
+
+    need=$(cat $tmp_file)
+    for trans in $need
+    do
+        sed -i "" 's/.$//' $trans # 将文件转为 Unix 风格
+        sed -i "" 's/	/    /g' $trans # 将 tab 替换成 4 个空格
+        sed -i "" 's/[ ]\+$//g' $trans # 删除行尾的空格
+    done
+done
+
+rm -rf tmp
