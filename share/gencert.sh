750d8c2b1b0789b0334bd92e20882a31fe8b93eb
Jerry Yang <hy0kle@gmail.com>
2016-03-18 16:37:23 +0800

    完善生成证书的脚本

--- share/gencert.sh
+++ share/gencert.sh
@@ -12,7 +12,7 @@ openssl genrsa -des3 -out $DOMAIN.key 1024
 
 echo "Create server certificate signing request..."
 
-SUBJECT="/C=US/ST=Mars/L=iTranswarp/O=iTranswarp/OU=iTranswarp/CN=$DOMAIN"
+SUBJECT="/C=US/ST=Mars/L=Pyramid/O=vipclub/OU=iTranswarp/CN=$DOMAIN/emailAddress=hy0kle@gmail.com"
 
 openssl req -new -subj $SUBJECT -key $DOMAIN.key -out $DOMAIN.csr
 
@@ -26,12 +26,12 @@ echo "Sign SSL certificate..."
 openssl x509 -req -days 3650 -in $DOMAIN.csr -signkey $DOMAIN.key -out $DOMAIN.crt
 
 echo "TODO:"
-echo "Copy $DOMAIN.crt to /etc/nginx/ssl/$DOMAIN.crt"
-echo "Copy $DOMAIN.key to /etc/nginx/ssl/$DOMAIN.key"
+echo "Copy $DOMAIN.crt to nginx/conf/ssl/$DOMAIN.crt"
+echo "Copy $DOMAIN.key to nginx/conf/ssl/$DOMAIN.key"
 echo "Add configuration in nginx:"
 echo "server {"
 echo "    ..."
 echo "    listen 443 ssl;"
-echo "    ssl_certificate     /etc/nginx/ssl/$DOMAIN.crt;"
-echo "    ssl_certificate_key /etc/nginx/ssl/$DOMAIN.key;"
+echo "    ssl_certificate     nginx/conf/ssl/$DOMAIN.crt;"
+echo "    ssl_certificate_key nginx/conf/ssl/$DOMAIN.key;"
 echo "}"
