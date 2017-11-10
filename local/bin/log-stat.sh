2c133a44eb88b62c990806236c6bec3242235f64
Jerry Yang <hy0kle@gmail.com>
2017-11-10 11:48:17 +0800

    log常用短语排行榜

---
+++ local/bin/log-stat.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# @describe: log常用短语排行榜
+# @author:   Jerry Yang(hy0kle@gmail.com)
+
+#set -x
+num=10
+if [ $# > 1 ]
+then
+    num=$1
+fi
+
+git log --oneline --color=never | awk '{
+    hash_len = length($1);
+    log_str  = substr($0, hash_len + 2);
+    sp_str = "Merge branch"
+    sp_len = length(sp_str);
+    if (length(log_str) > 0 && sp_str != substr(log_str, 1, sp_len)) {
+        print(log_str);
+    }
+}' | sort | uniq -c | sort -nr -k1 | head -n $num
+# vim:set ts=4 sw=4 et fdm=marker:
+
